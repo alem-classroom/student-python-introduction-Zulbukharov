d986c1750c866eb12adec9e5b50f720fd4a40e5d
Saken <35443199+sakenism@users.noreply.github.com>
2020-07-13 16:37:04 +0600

    Initial commit

---
+++ variables-and-types/calculator.py
@@ -0,0 +1,16 @@
+def add(first, second):
+    # return sum of first and second arguments
+
+def sub(first, second):
+    # return subtraction of first and second arguments
+
+def multiply(first, second):
+    # return multiplicatiom of first and second arguments
+
+def div_with_floor(first, second):
+    # return division with floor of first and second arguments
+
+
+def div_without_floor(first, second):
+    # return division without floor of first and second arguments
+
