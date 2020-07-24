9005ff04ed735185eb71a30bdcfdd0d50f6e460b
Zulbukharov <l1pt0n1905@gmail.com>
2020-07-24 13:29:07 +0600

    lists

--- lists/lists.py
+++ lists/lists.py
@@ -1,17 +1,25 @@
 def size_of_list(list):
+    return len(list)
     # return size of list
 
 def add_elem_to_list(list, elem):
+    list.append(elem)
+    return list
     # add elem to list and return the list
 
 def delete_elem_from_list(list, index = -1):
+    list.pop(index)
+    return list
     # delete element from list, such that its index is index
 
 def count_elements_in_list(list, x):
+    return list.count(x)
     # count elements in the list that are equal to x and return the count
 
 def sort_list(list):
+    return list.sort()
     # return sorted list
 
 def reverse(list):
+    return list.reverse()
     # return reversed list
